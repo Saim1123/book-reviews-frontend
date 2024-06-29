d00f13aee9fef4c9375d7d6310a83be5d55504ab
SaimTahir <127545146+Saim1123@users.noreply.github.com>
2024-06-29 18:10:15 +0500

    used theme color.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -27,35 +27,35 @@ const Login = () => {
   return (
     <div className="flex justify-center items-center h-screen bg-gray-200">
       <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <h1 className="text-2xl mb-4 text-center">Login</h1>
+        <h1 className="text-2xl mb-4 text-blue-600 text-center">Login</h1>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
+            <label className="block text-blue-600 text-sm font-bold mb-2">
               Email
             </label>
             <input
               type="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-blue-600 leading-tight focus:outline-none focus:shadow-outline"
               placeholder="Email..."
             />
           </div>
           <div className="mb-6">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
+            <label className="block text-blue-600 text-sm font-bold mb-2">
               Password
             </label>
             <input
               type="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-blue-600 mb-3 leading-tight focus:outline-none focus:shadow-outline"
               placeholder="Password..."
             />
           </div>
           <div className="text-center">
             <button
-              className="bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              className="bg-gradient-to-r from-blue-500 to-indigo-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="submit"
             >
               Login
